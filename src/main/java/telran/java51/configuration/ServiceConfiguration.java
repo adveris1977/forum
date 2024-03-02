1bd7cb8038874c8b7e4dea3532f04790ca717213
Alex Dveris <adveris77@gmail.com>
2024-03-02 16:05:21 +0200

    Change5

--- src/main/java/telran/java51/configuration/ServiceConfiguration.java
+++ src/main/java/telran/java51/configuration/ServiceConfiguration.java
@@ -5,10 +5,12 @@ import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ServiceConfiguration {
-	
+
 	@Bean
 	public ModelMapper getModelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
@@ -17,6 +19,11 @@ public class ServiceConfiguration {
 					.setFieldAccessLevel(AccessLevel.PRIVATE)
 					.setMatchingStrategy(MatchingStrategies.STRICT);
 		return modelMapper;
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
